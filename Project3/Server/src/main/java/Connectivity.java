f41a4e9b61af9c6fd74ec1a3f518251e8e219d96
jgongof <113205744+jgongof@users.noreply.github.com>
2023-11-24 19:45:18 -0600

    Update Connectivity.java

--- Project3/Server/src/main/java/Connectivity.java
+++ Project3/Server/src/main/java/Connectivity.java
@@ -3,15 +3,31 @@ import java.util.ArrayList;
 public class Connectivity implements Serializable{
     private static final long serialVersionUID = 1L;
 
-    int categoryWordLength;
-    int attempts = 6;
+    int dessertWordLength;
+    int ftWordLength;
+    int citiesWordLength;
+
+
+    int attempts;
+
+    String correctDessert;
+    String correctFairyTale;
+    String correctCity;
+
+
 
     boolean gotCorrectLetter;
     boolean gotCorrectWord;
 
+    int categoryNumber;
+
+    String playerActivity;
+
+    String category;
+
     char userLetter;
 
-    int desserts_attempts = 3;
-    int fairytales_attempts = 3;
-    int cities_attempts = 3;
+    int desserts_attempts;
+    int fairytales_attempts;
+    int cities_attempts;
 }
